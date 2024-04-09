09dafc3199a7479d0ea9427e19a8c05bbe4a41b2
jhutchings99 <jeremyhutchings99@gmail.com>
2024-04-08 18:20:44 -0600

    missed one

--- client/app/map/page.tsx
+++ client/app/map/page.tsx
@@ -4,14 +4,17 @@ import Map from "../components/Map/Map";
 import Navbar from "../components/Navbar/Navbar";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 
 export default function MapPage() {
   const { data: session } = useSession();
   const router = useRouter();
 
-  if (!session?.user) {
-    router.push("/");
-  }
+  useEffect(() => {
+    if (!session?.user) {
+      router.push("/");
+    }
+  });
 
   return (
     <main className="flex px-52">
